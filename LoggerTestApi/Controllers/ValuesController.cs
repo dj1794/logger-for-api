075697efefe0cfaf27055d020da8dea626fd4b30
djoshi1794 <dinesh.joshi1794@gmail.com>
2019-05-28 22:08:21 +0530

    Logger test api

---
+++ LoggerTestApi/Controllers/ValuesController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using log4net.Repository.Hierarchy;
+using log4net.Core;
+using log4net.Repository;
+using Microsoft.Extensions.Logging;
+using log4net;
+
+namespace LoggerTestApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+       
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var logger = LogManager.GetLogger(typeof(Program));
+            var j = "b";
+            try
+            {
+                Convert.ToInt32(j);
+            }
+            catch (Exception e)
+            {
+                logger.Error("Logger is working!!! " + e.Message);
+            }
+            
+            return new string[] { j, "Test" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+            Convert.ToInt32(value);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ LoggerTestApi/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Reflection;
+using log4net.Config;
+using log4net;
+
+namespace LoggerTestApi
+{
+    public class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+            .ConfigureLogging((hostingContext, logging) =>
+            {
+                // The ILoggingBuilder minimum level determines the
+                // the lowest possible level for logging. The log4net
+                // level then sets the level that we actually log at.
+                logging.AddLog4Net();
+                logging.SetMinimumLevel(LogLevel.Debug);
+            })
+                .Build();
+
+    }
+}
